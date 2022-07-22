70dfde3c40a70c96fa06b20faa48969157ffa209
Benjamin Correia <benjamin.j.correia@gmail.com>
2022-07-21 23:46:31 -0700

    Remove daemon

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,7 +7,6 @@
 
 use mdbapi::*;
 use std::{fs::File, io::Read, path::PathBuf, vec::Vec};
-use sysinfo::{ProcessExt, System, SystemExt};
 use tauri::{generate_handler, Manager, State};
 
 mod mdbapi;
@@ -192,7 +191,6 @@ fn get_binary_config() -> BinaryConfig {}
 fn get_binary_config() -> BinaryConfig {}
 
 fn main() {
-    enforce_daemon();
     tauri::Builder::default()
         .invoke_handler(generate_handler![
             //TAG API
@@ -222,31 +220,4 @@ fn main() {
         })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
-}
-
-fn enforce_daemon() {
-    let bcfg = get_binary_config();
-    let sys = System::new_all();
-    let memes = sys
-        .processes()
-        .iter()
-        .filter(|(_, proc)| proc.name().starts_with("meme-db-daemon"));
-    let count = memes.clone().count();
-    if count == 1 {
-        println!("Daemon already started.");
-        return;
-    }
-    if count > 1 {
-        println!("Too many deamons running! Killing imposters (sussy)!");
-        memes.for_each(|(_, proc)| {
-            proc.kill();
-        });
-    }
-
-    println!("Starting daemon!");
-    //TODO: FE-BE can still run without daemon, just some features won't work.
-    //TODO: process will block termination waiting for the daemon to terminate
-    std::process::Command::new(bcfg.daemon_location)
-        .spawn()
-        .expect("Can't start daemon");
 }
